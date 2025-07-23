d1f24968bd30ded31b63c623f0315c7c938e1e16
Aman11b <amansingh1100b@gmail.com>
2025-07-23 08:39:00 +0530

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'vgreen':"#44a659"
+      }
+    },
   },
   plugins: [],
 };
